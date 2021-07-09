2a88b1e21cd626e76908c6f9b287984693126439
thuan <phamtanthuan2311@gmail.com>
2021-07-09 22:02:50 +0700

    refactor: new page for live chat, self host font (close #13, close #19)

---
+++ pages/liveChat.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="live-container">
+    <ChannelStats />
+    <LiveChat />
+  </div>
+</template>
+
+<script>
+import ChannelStats from '@/components/ChannelStats'
+import LiveChat from '@/components/LiveChat'
+export default {
+  components: [ChannelStats, LiveChat],
+  layout: null,
+}
+</script>
+
+<style scoped>
+.live-container {
+  background-color: #111111;
+}
+</style>
